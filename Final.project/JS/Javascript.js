5112935ae4ba15083d90ea521b921a4b3cdb0a0d
FernandaPadovan <99438564+FernandaPadovan@users.noreply.github.com>
2022-05-11 02:26:02 +0100

    inicial commit

---
+++ Final.project/JS/Javascript.js
@@ -0,0 +1,25 @@
+
+// <!-- Script function for Modal Log in and Register -->
+
+          function updatephrase(phrase)
+          {
+              console.log(phrase);
+              $('.modal-phrase').text(phrase);
+          }
+
+
+// <!-- Script function for Modal checkout -->
+
+          function updatePrice(price)
+          {
+              console.log(price);
+              $('.modal-price').text('$' + price);
+          }
+
+
+
+
+
+
+
+
